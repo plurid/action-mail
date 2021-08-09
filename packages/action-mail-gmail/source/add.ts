2a406cfd4a916ca2c36d9e90560d0ac8887df7aa
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-09 11:48:45 +0300

    action-mail-gmail > dev: addPage

--- packages/action-mail-gmail/source/add.ts
+++ packages/action-mail-gmail/source/add.ts
@@ -25,40 +25,40 @@ function buildAddCard() {
     const onChangeAction = CardService.newAction().setFunctionName('onChangeAddPage');
 
     const toMail = CardService.newTextInput()
-      .setFieldName("toMail")
-      .setOnChangeAction(onChangeAction)
-      .setTitle("to mail");
+        .setFieldName('toMail')
+        .setOnChangeAction(onChangeAction)
+        .setTitle('to mail');
 
     const endpoint = CardService.newTextInput()
-      .setFieldName("endpoint")
-      .setOnChangeAction(onChangeAction)
-      .setTitle("endpoint");
+        .setFieldName('endpoint')
+        .setOnChangeAction(onChangeAction)
+        .setTitle('endpoint');
 
     var endpointType = CardService.newSelectionInput()
         .setType(CardService.SelectionInputType.RADIO_BUTTON)
-        .setTitle("endpoint type")
-        .setFieldName("endpointType")
+        .setTitle('endpoint type')
+        .setFieldName('endpointType')
         .setOnChangeAction(onChangeAction)
-        .addItem("REST", "endpointTypeRest", true)
-        .addItem("GraphQL", "endpointTypeGraphql", false);
+        .addItem('REST', 'endpointTypeRest', true)
+        .addItem('GraphQL', 'endpointTypeGraphql', false);
 
     const spacer = CardService.newTextInput()
-      .setFieldName("spacer")
-      .setOnChangeAction(onChangeAction)
-      .setTitle("spacer");
+        .setFieldName('spacer')
+        .setOnChangeAction(onChangeAction)
+        .setTitle('spacer');
 
     const token = CardService.newTextInput()
-      .setFieldName("token")
-      .setOnChangeAction(onChangeAction)
-      .setTitle("token");
+        .setFieldName('token')
+        .setOnChangeAction(onChangeAction)
+        .setTitle('token');
 
     const tokenType = CardService.newSelectionInput()
         .setType(CardService.SelectionInputType.RADIO_BUTTON)
-        .setTitle("token type")
-        .setFieldName("tokenType")
+        .setTitle('token type')
+        .setFieldName('tokenType')
         .setOnChangeAction(onChangeAction)
-        .addItem("payload", "tokenTypePayload", true)
-        .addItem("bearer", "tokenTypeBearer", false);
+        .addItem('payload', 'tokenTypePayload', true)
+        .addItem('bearer', 'tokenTypeBearer', false);
 
     const addAction = CardService.newAction().setFunctionName('submitAddPage');
     const addButton = CardService.newImageButton()
@@ -67,18 +67,18 @@ function buildAddCard() {
 
 
     const section = CardService.newCardSection()
-      .addWidget(banner)
-      .addWidget(toMail)
-      .addWidget(endpoint)
-      .addWidget(endpointType)
-      .addWidget(spacer)
-      .addWidget(token)
-      .addWidget(tokenType)
-      .addWidget(addButton)
+        .addWidget(banner)
+        .addWidget(toMail)
+        .addWidget(endpoint)
+        .addWidget(endpointType)
+        .addWidget(spacer)
+        .addWidget(token)
+        .addWidget(tokenType)
+        .addWidget(addButton);
 
 
     return CardService.newCardBuilder()
-      .addSection(section)
-      .build();
+        .addSection(section)
+        .build();
 }
 // #endregion module
