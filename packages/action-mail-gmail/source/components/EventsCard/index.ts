6cbd9ced2db86e7c869af7395f4c0224b524a9cf
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-16 13:39:22 +0300

    action-mail-gmail > dev: components/EventsCard

--- packages/action-mail-gmail/source/components/EventsCard/index.ts
+++ packages/action-mail-gmail/source/components/EventsCard/index.ts
@@ -3,12 +3,25 @@
     import {
         BANNER_ICON_URL,
     } from '~data/constants';
+
+    import {
+        propertiesGet,
+    } from '~services/properties';
     // #endregion external
 // #endregion imports
 
 
 
 // #region module
+const getEvents = (
+    toMail: string,
+) => {
+    const events: any[] = [];
+
+    return events;
+}
+
+
 const EventsCard = (
     id: string,
 ) => {
@@ -16,8 +29,39 @@ const EventsCard = (
         .setImageUrl(BANNER_ICON_URL);
 
 
+    const configData = propertiesGet(id);
+    if (!configData) {
+        const header = CardService.newTextParagraph()
+            .setText(`<b>No Events</b>`);
+
+        const section = CardService.newCardSection()
+            .addWidget(banner)
+            .addWidget(header)
+
+        return CardService.newCardBuilder()
+            .addSection(section)
+            .build();
+    }
+
+
+    const {
+        toMail,
+    } = configData;
+
+
+    const header = CardService.newTextParagraph()
+        .setText(`<b>Events for ${toMail}</b>`);
+
+
     const section = CardService.newCardSection()
         .addWidget(banner)
+        .addWidget(header);
+
+
+    const events = getEvents(toMail);
+    for (const event of events) {
+        section.addWidget(event);
+    }
 
 
     return CardService.newCardBuilder()
