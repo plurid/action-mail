0d717067ee51f7f09040444d0528484667462ae8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-18 11:27:31 +0300

    fixtures/test-endpoint > dev: source

--- fixtures/test-endpoint/source/index.ts
+++ fixtures/test-endpoint/source/index.ts
@@ -4,13 +4,89 @@
     import {
         json as jsonParser,
     } from 'body-parser';
+
+    import CryptoJS from 'crypto-js';
+    import crypto from 'crypto';
     // #endregion libraries
+
+
+    // #region internal
+    import {
+        privateKey,
+    } from './keys';
+    // #endregion internal
 // #endregion imports
 
 
 
 // #region module
 const PORT = 44555;
+
+export interface Crypted {
+    text: string;
+    aes: string;
+}
+
+
+const decryptAes = (
+    aes: string,
+    privateKey: string,
+) => {
+    const aesKey = crypto.privateDecrypt(
+        {
+            key: privateKey,
+            padding: crypto.constants.RSA_PKCS1_PADDING,
+        },
+        Buffer.from(aes, 'base64'),
+    );
+
+    return aesKey.toString();
+}
+
+const decryptLoad = (
+    aesKey: string,
+    load: string,
+) => {
+    const aesBytes = CryptoJS.AES.decrypt(load, aesKey.toString());
+    const clearText = JSON.parse(aesBytes.toString(CryptoJS.enc.Utf8));
+
+    return clearText;
+}
+
+
+const decrypt = (
+    crypted: Crypted,
+): string => {
+    const {
+        aes,
+        text,
+    } = crypted;
+
+    const aesKey = decryptAes(
+        aes,
+        privateKey,
+    );
+
+    const load = decryptLoad(
+        aesKey,
+        text,
+    );
+
+    return load;
+}
+
+
+const handleRequest = async (
+    metadata: Crypted,
+    data: Crypted,
+) => {
+    const clearMetadata = decrypt(metadata);
+    console.log('clearMetadata', clearMetadata);
+
+    const clearData = decrypt(data);
+    console.log('clearData', clearData);
+}
+
 
 
 const main = async () => {
@@ -19,17 +95,36 @@ const main = async () => {
     server.use(jsonParser());
 
 
-    server.post('/rest', (request, response) => {
+    server.post('/rest', async (request, response) => {
+        response.end();
+
         console.log('rest endpoint hit', request.body);
 
-        response.end();
+        const {
+            metadata,
+            data,
+        } = request.body;
+
+        await handleRequest(
+            metadata,
+            data,
+        );
     });
 
-    server.post('/graphql', (request, response) => {
-        console.log('graphql endpoint hit', request.body);
-        console.log('input', request.body.variables.input);
-
+    server.post('/graphql', async (request, response) => {
         response.end();
+
+        console.log('graphql endpoint hit', request.body);
+
+        const {
+            metadata,
+            data,
+        } = request.body.variables.input;
+
+        await handleRequest(
+            metadata,
+            data,
+        );
     });
 
 
